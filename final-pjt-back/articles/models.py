02a4cc855d4321609d1a4442d8777cb5207d4dbd
김해인 <khi762@naver.com>
2023-11-16 17:47:12 +0900

    feat: articles, movies 모델 추가

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from movies.models import Movie
+from django.conf import settings
 
 # Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ref_movie = models.ForeignKey(Movie, blank=True, null=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(blank=True, upload_to='article/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
