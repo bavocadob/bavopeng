82f707aa93f90e125fb5bcee2710288e64a935c2
김해인 <khi762@naver.com>
2023-11-17 12:50:55 +0900

    feat: article read, create

---
+++ final-pjt-back/articles/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Article, Comment
+from accounts.serializers import UserSimpleSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('article', 'user',)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True, read_only=True)
+    comment_cnt = serializers.IntegerField(source='comment_set.count', read_only=True)
+    user = UserSimpleSerializer(read_only=True)
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class CommentSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = ('id',)
+
+    # comment_set = CommentSerializer(many=True, read_only=True)    # 선언을 안해도 source는 가지고 올 수 있는 건가?
+    comment_cnt = serializers.IntegerField(source='comment_set.count', read_only=True)
+    user = UserSimpleSerializer(read_only=True)
+    
+    class Meta:
+        model = Article
+        fields = ('id', 'user', 'title', 'created_at', 'comment_cnt',)
