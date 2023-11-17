e718ae966815568c822a85e5e9ee27c5ced24f7e
bavocadob <1ksy3629@gmail.com>
2023-11-17 13:45:11 +0900

    feat:review 관련 crud

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
-from .models import Genre, Movie
+from .models import Genre, Movie, Director, Actor, Review
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = '__all__'
 
+
+class MovieListSerializer(serializers.ModelSerializer):
+    pass
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class DirectorSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Director
+            fields = '__all__'
+
+
+    class ActorSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Actor
+            fields = '__all__'
+
+
+    actors = ActorSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True, read_only=True)
+    directors = DirectorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'original_title', 'certification', 'overview','release_date','runtime' , 'genres', 'backdrop_path', 'poster_path',
+                  'actors', 'directors', 'trailer')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('movie', 'user')
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,12 +1,22 @@
 # from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 from .serializers import GenreSerializer
-from .models import Genre, Movie, Actor, Director
+from .models import Genre, Movie, Actor, Director, Review
+from .serializers import MovieSerializer, ReviewSerializer
 import requests
 import re
+
+
+
 
 API_KEY = settings.API_KEY
 
@@ -105,8 +115,8 @@ def insert_movies(request):
     response = requests.get(BASE_URL, params=params).json()
     
     # 검색한 조건의 페이지 개수만큼, 페이지 1씩 올려가면서 api 요청
-    for page in range(1, response['total_pages'] + 1):
-    # for page in range(281, 290):
+    # for page in range(1, response['total_pages'] + 1):
+    for page in range(300, 311):
         params['page'] = page
 
         response = requests.get(BASE_URL, params=params).json()
@@ -121,6 +131,12 @@ def insert_movies(request):
             movie_fields = {k: v for k, v in detail_response.items() if k in [f.name for f in Movie._meta.get_fields() if not isinstance(f, models.ManyToManyField)]}
             movie_fields['trailer'] = get_trailer(detail_response)
             movie_fields['certification'] = get_certification(detail_response)
+            if movie_fields['backdrop_path'] is None:
+                movie_fields['backdrop_path'] = ''
+
+            if movie_fields['poster_path'] is None:
+                movie_fields['poster_path'] = ''
+
             movie_obj = Movie.objects.create(**movie_fields)
 
             add_genres(movie_obj, detail_response['genres'])
@@ -133,19 +149,146 @@ def insert_movies(request):
 
 
 def insert_genres(request):
-    url = "https://api.themoviedb.org/3/genre/movie/list"
+    # url = "https://api.themoviedb.org/3/genre/movie/list"
 
-    params = {
-        'api_key': settings.API_KEY,
-        'language': 'ko-KR',
-    }
-
-    response = requests.get(url, params=params).json()
-    genres = response['genres']
+    # params = {
+    #     'api_key': settings.API_KEY,
+    #     'language': 'ko-KR',
+    # }
     
-    for genre in genres:
+    # response = requests.get(url, params=params).json()
+    # genres = response['genres']
+    genres = {
+        "results" :[
+        {
+        "id": 28,
+        "name": "액션"
+        },
+        {
+        "id": 12,
+        "name": "모험"
+        },
+        {
+        "id": 16,
+        "name": "애니메이션"
+        },
+        {
+        "id": 35,
+        "name": "코미디"
+        },
+        {
+        "id": 80,
+        "name": "범죄"
+        },
+        {
+        "id": 99,
+        "name": "다큐멘터리"
+        },
+        {
+        "id": 18,
+        "name": "드라마"
+        },
+        {
+        "id": 10751,
+        "name": "가족"
+        },
+        {
+        "id": 14,
+        "name": "판타지"
+        },
+        {
+        "id": 36,
+        "name": "역사"
+        },
+        {
+        "id": 27,
+        "name": "공포"
+        },
+        {
+        "id": 10402,
+        "name": "음악"
+        },
+        {
+        "id": 9648,
+        "name": "미스터리"
+        },
+        {
+        "id": 10749,
+        "name": "로맨스"
+        },
+        {
+        "id": 878,
+        "name": "SF"
+        },
+        {
+        "id": 10770,
+        "name": "TV 영화"
+        },
+        {
+        "id": 53,
+        "name": "스릴러"
+        },
+        {
+        "id": 10752,
+        "name": "전쟁"
+        },
+        {
+        "id": 37,
+        "name": "서부"
+        }
+    ]
+    }
+    for genre in genres['results']:
         serializer = GenreSerializer(data=genre)
         if serializer.is_valid():
             serializer.save()
 
-    return JsonResponse(response)
+    return JsonResponse(genres)
+
+
+
+@api_view(['GET'])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
+
+
+@api_view(['GET','POST'])
+def movie_review(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    
+    if request.method == 'POST':  # 게시글에 대한 리뷰 작성
+        serializer = ReviewSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(movie=movie, user=request.user)
+            return Response(serializer.data)
+    elif request.method == 'GET':  # 게시글에 대한 리뷰 조회
+        reviews = movie.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_review_detail(request, movie_pk, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    
+
+    if request.method == 'GET': # 리뷰 단일 조회
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    elif request.user and request.user.is_authenticated and request.user == review.user:  # 로그인한 리뷰 작성 유저만 접근 가능
+        if request.method == 'PUT':  # 리뷰 수정
+            serializer = ReviewSerializer(data=request.data, instance=review, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+        elif request.method == 'DELETE':  # 리뷰 삭제
+            review.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
+
+
